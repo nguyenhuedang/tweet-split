30ae785ec31eb2d1dbd04409c111cd2162c98f6e
nguyenhuedang <dang.nguyen@tiki.vn>
2017-07-17 14:15:39 +0700

    Add tests and coverage for TweetSplit, refactor tweet-input
    - Report the coverage of testing to measure the quality using
    coverall.io
    - Refactor the tweet-input to remove redundant property valid (= true
    if the message is valid), the property _error is now used to detect the
    message is valid or not.
    - Add test for all components

--- wct.conf.js
+++ wct.conf.js
@@ -4,35 +4,37 @@ module.exports = {
     'local': {
       'browsers': ['chrome'],
     },
+    'istanbul': {
+      verbose: true,
+      dir: 'logs/coverage',
+      reporters: ['text-summary', 'lcov'],
+      include: ['/src/**'],
+      exclude: ['**/test/**'],
+    },
+    'xunit-reporter': {
+      output: 'logs/test-results.xml',
+    },
     'sauce': {
       'disabled': true,
       'browsers': [{
-        'browserName': 'microsoftedge',
+        'browserName': 'chrome',
         'platform': 'Windows 10',
         'version': '',
       }, {
-        'browserName': 'internet explorer',
+        'browserName': 'chrome',
         'platform': 'Windows 8.1',
-        'version': '11',
+        'version': 'beta',
       }, {
         'browserName': 'safari',
         'platform': 'OS X 10.11',
         'version': '10',
       }, {
-        'browserName': 'safari',
+        'browserName': 'chrome',
         'platform': 'OS X 10.11',
-        'version': '9',
-      }, {
-        'browserName': 'chrome',
-        'platform': 'Linux',
-        'version': '',
-      }, {
-        'browserName': 'chrome',
-        'platform': 'Windows 10',
         'version': 'beta',
       }, {
         'browserName': 'chrome',
-        'platform': 'Windows 10',
+        'platform': 'OS X 10.11',
         'version': '',
       }, {
         'browserName': 'firefox',
